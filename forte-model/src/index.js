2aec52b3c295eebc9cdc0d9b2123be477266a98f
Oh Jinkyun <tintypemolly@gmail.com>
2017-06-04 18:43:42 +0900

    define model

---
+++ forte-model/src/index.js
@@ -0,0 +1,35 @@
+import Sequelize from 'sequelize';
+import {stations} from '../../forte-cron';
+
+const sequelize = new Sequelize('forte', 'forte', 'BDfdssx4BqthXFxpmWE5');
+
+const Station = sequelize.define('station', {
+    'oper': {type: Sequelize.STRING},
+    'addr': {type: Sequelize.STRING, unique: true},
+    'mangName': {type: Sequelize.STRING},
+    'item': {type: Sequelize.STRING},
+    'stationName': {type: Sequelize.STRING},
+    'year': {type: Sequelize.INTEGER},
+    'dmX': {type: Sequelize.DOUBLE},
+    'dmY': {type: Sequelize.DOUBLE},
+    'uuid': {type: Sequelize.STRING, primaryKey: true},
+});
+
+const Observation = sequelize.define('observation', {
+    'station_id': {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        references: {model: Station, key: 'uuid'},
+    },
+    'so2': {type: Sequelize.DOUBLE},
+    'co': {type: Sequelize.DOUBLE},
+    'o3': {type: Sequelize.DOUBLE},
+    'no2': {type: Sequelize.DOUBLE},
+    'pm10': {type: Sequelize.DOUBLE},
+    'pm25': {type: Sequelize.DOUBLE},
+    'last_modified': {
+        type: Sequelize.DATE,
+        defaultValue: Date.now,
+        onUpdate: Date.now,
+    },
+});
