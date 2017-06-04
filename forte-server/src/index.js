df86f1263f418864ca5fea9dc9b2a22c13d12c6e
Oh Jinkyun <tintypemolly@gmail.com>
2017-06-04 21:50:57 +0900

    server db con test

--- forte-server/src/index.js
+++ forte-server/src/index.js
@@ -4,5 +4,12 @@ import {Station, Observation} from '../../forte-model';
 const app = express();
 
 app.get('/', (req, res) => {
+    Station.findAll({include: Observation}).then(stations => {
+        const data = stations.map(s => s.get({plain: true}));
+        res.send(JSON.stringify(data));
+    });
+});
 
+app.listen(3000, function() {
+    console.log('listening on port 3000!');
 });
