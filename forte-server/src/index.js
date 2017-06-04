4c56dfac61fd6c0d847f8bfb2fb44a0dcb2c496c
Oh Jinkyun <tintypemolly@gmail.com>
2017-06-04 22:32:22 +0900

    add more route

--- forte-server/src/index.js
+++ forte-server/src/index.js
@@ -3,12 +3,15 @@ import {Station, Observation} from '../../forte-model';
 
 const app = express();
 
-app.get('/', (req, res) => {
+const getData = (req, res) => {
     Station.findAll({include: Observation}).then(stations => {
         const data = stations.map(s => s.get({plain: true}));
         res.send(JSON.stringify(data));
     });
-});
+};
+
+app.get('/', getData);
+app.get('/api', getData);
 
 app.listen(3000, function() {
     console.log('listening on port 3000!');
