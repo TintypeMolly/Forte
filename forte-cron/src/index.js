718070cb610abf2fbe76cfde8997b286c93aa27e
Oh Jinkyun <tintypemolly@gmail.com>
2017-06-04 19:24:54 +0900

    oops

--- forte-cron/src/index.js
+++ forte-cron/src/index.js
@@ -1,7 +1,7 @@
 import stations from './stations.json';
 import fetchStation from './fetch_station';
 import {Station, Observation} from '../../forte-model';
-import initDB from 'init_db';
+import initDB from './init_db';
 
 initDB();
 
