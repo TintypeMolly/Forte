32330ede8b2dcaa63f32ce091bc39cec93c1f75b
Oh Jinkyun <tintypemolly@gmail.com>
2017-06-06 02:00:02 +0900

    working on credit page

--- forte-client/src/components/credit/index.js
+++ forte-client/src/components/credit/index.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 
-class Credit extends Component {
-  render() {
-    return (
-      <div>This is credit page</div>
-    );
-  }
-}
+import forteImage from './forte_avatar.png';
+
+
+const Credit = () => {
+  const cardStyle = {marginBottom: 10, marginRight: 10, display: 'inline-block'};
+  return (
+    <div style={{padding: 10}}>
+      <h2>Members</h2>
+      <Card style={cardStyle}>
+        <CardHeader title="오진균" subtitle="2010147101" avatar={forteImage}/>
+        <CardText>시스템 엔지니어링, 프로그래밍 담당</CardText>
+      </Card>
+      <Card style={cardStyle}>
+        <CardHeader title="박이재" subtitle="2016147546" avatar={forteImage}/>
+        <CardText>프로젝트 관리 담당</CardText>
+      </Card>
+      <Card style={cardStyle}>
+        <CardHeader title="윤희준" subtitle="2016147525" avatar={forteImage}/>
+        <CardText>문서 및 PPT 작성 담당</CardText>
+      </Card>
+      <Card style={cardStyle}>
+        <CardHeader title="황경윤" subtitle="2016147503" avatar={forteImage}/>
+        <CardText>프로그래밍, 리소스 관리 담당</CardText>
+      </Card>
+      <h2>Open Source Usage Acknowledge</h2>
+      <p>TODO</p>
+    </div>
+  );
+};
 
 export default Credit;
