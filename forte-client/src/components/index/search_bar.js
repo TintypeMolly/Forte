6ca6a73b77be7085ee86f2954e086257641fd12b
Oh Jinkyun <tintypemolly@gmail.com>
2017-06-05 18:31:19 +0900

    geocoding

--- forte-client/src/components/index/search_bar.js
+++ forte-client/src/components/index/search_bar.js
@@ -3,12 +3,32 @@ import TextField from 'material-ui/TextField';
 import IconButton from 'material-ui/IconButton';
 import SearchIcon from 'material-ui/svg-icons/action/search';
 import MyLocationIcon from 'material-ui/svg-icons/maps/my-location';
+import geocoder from 'geocoder';
 
 import store from '../../store';
 import {setMapCenter, setCurrentStation} from '../../actions';
 import {findNearestActiveStation} from './util'
 
+const searchAddress = (stations, addr) => {
+  const option = {key: 'AIzaSyBUBNOFCXmHCkj2LcmI2f8tmYRN9-QPAqA', language: 'ko'};
+  geocoder.geocode(addr, (err, data) => {
+    console.log(data);
+    if (data.status === 'OK') {
+      const location = data.results[0].geometry.location;
+      const nearestStation = findNearestActiveStation(stations, location.lat, location.lng);
+      store.dispatch(setCurrentStation(nearestStation));
+      store.dispatch(setMapCenter(location.lat, location.lng));
+    } else {
+      alert('주소 검색에 실패했습니다.');
+    }
+  }, option);
+};
+
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.textFieldValue = '';
+  }
   render() {
     const textFieldStyle = {
       flex: 1,
@@ -31,10 +51,26 @@ class SearchBar extends Component {
         alert("Geolocation을 사용할 수 없는 브라우저입니다.");
       }
     };
+    const onKeyDown = e => {
+      if (e.keyCode === 13) {
+        searchAddress(this.props.stations, this.textFieldValue);
+      }
+    };
+    const onSearchTap = () => {
+      searchAddress(this.props.stations, this.textFieldValue);
+    };
+    const onTextChange = (e, newValue) => {
+      this.textFieldValue = newValue;
+    };
     return (
       <div style={{display: 'flex'}}>
-        <TextField style={textFieldStyle} hintText="주소를 입력하세요"/>
-        <IconButton style={iconButtonStyle}>
+        <TextField
+          style={textFieldStyle}
+          hintText="주소를 입력하세요"
+          onKeyDown={onKeyDown}
+          onChange={onTextChange}
+        />
+        <IconButton style={iconButtonStyle} onTouchTap={onSearchTap}>
           <SearchIcon/>
         </IconButton>
         <IconButton style={iconButtonStyle} onTouchTap={onMyLocationTap}>
