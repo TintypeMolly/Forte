9cbec52d7a6871a523741a78e9995b16af173ad2
Oh Jinkyun <tintypemolly@gmail.com>
2017-06-09 13:48:39 +0900

    cron

--- forte-cron/src/index.js
+++ forte-cron/src/index.js
@@ -3,34 +3,37 @@ import fetchStation from './fetch_station';
 import {Station, Observation} from '../../forte-model';
 
 const updateObservations = async () => {
-    for (const station of stations) {
-        let observation;
-        let retry = 0;
-        while (true) {
-            try {
-                observation = await fetchStation(station);
-                break;
-            } catch(err) {
-                console.error(err);
-                console.log('retry...');
-                retry += 1;
-                if (retry > 10) {
-                    console.log('something is srsly wrong');
-                    process.exit(1);
-                }
-            }
+  for (const station of stations) {
+    let observation;
+    let retry = 0;
+    while (true) {
+      try {
+        observation = await fetchStation(station);
+        break;
+      } catch (err) {
+        console.error(err);
+        console.log('retry...');
+        retry += 1;
+        if (retry > 10) {
+          console.log('something is srsly wrong');
+          process.exit(1);
         }
-        await Observation.findOrCreate({
-            where: {station_id: station.uuid},
-            defaults: observation,
-        });
-        // sleep 1 second
-        await new Promise(resolve => setTimeout(resolve, 1000));
+      }
     }
+    const dbObservation = await Observation.findOne({where: {station_id: station.uuid}});
+    if (dbObservation) {
+      dbObservation.update(observation);
+    } else {
+      Observation.create(observation);
+    }
+
+    // sleep 1 second
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  }
 };
 
 updateObservations().then(_ => console.log('done'));
 
 export {
-    stations,
+  stations,
 };
