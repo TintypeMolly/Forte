0f226a79d011a5741c4e4f4c6e381c2d12bdd779
Oh Jinkyun <tintypemolly@gmail.com>
2017-06-12 18:49:00 +0900

    about page

--- forte-client/src/components/about/index.js
+++ forte-client/src/components/about/index.js
@@ -5,6 +5,7 @@ import mobileScreenshot from './mobile.png';
 import mediumScreenshot from './medium.png';
 import bigScreenshot from './big.png';
 import './about.css';
+import structureImage from './structure.png';
 
 const About = () => {
   return (
@@ -29,7 +30,14 @@ const About = () => {
         </p>
         <h3>기능 설명</h3>
         <h4>구조</h4>
-        <p>TODO</p>
+        <img src={structureImage}/>
+        <ul>
+          <li>forte-cron은 한국환경공단 대기오염 서비스(Air Korea)로부터 모든 관측소의 대기질 정보를 받아와 DB를 갱신합니다.</li>
+          <li>forte-server는 DB에 기록된 정보를 json으로 제공하는 단순한 웹 서버입니다.</li>
+          <li>forte-client는 현재 이 페이지를 표현하고 있는 웹페이지를 표현합니다. 웹 페이지의 생김새만 담고 있고, Nginx에 의해 정적인 파일로 서빙됩니다.</li>
+          <li>사용자가 forte-client를 보고 있으면 그 웹페이지에서 Ajax를 통해 forte-server의 현재 관측값을 받아와서 미세먼지 정보를 표현합니다.</li>
+          <li>forte-server에 대한 접속과 forte-client에 대한 접속은 Nginx에 의해서 라우팅되어 구분됩니다.</li>
+        </ul>
         <h4>Geolocation API 사용</h4>
         <p>
           작아서 보기도 힘든 지도를 어렵게 클릭하거나 도시 단위의 대략적인 정보를 볼 필요가 없습니다.
@@ -54,7 +62,12 @@ const About = () => {
           별도의 모바일 페이지를 만들지 않고, 한 페이지 내에서 스크린 크기에 따라 반응하여 레이아웃을 적절하게 실시간으로 변경합니다.
         </p>
         <h4>Single Page App</h4>
-        <p>TODO</p>
+        <p>
+          <a href="https://facebook.github.io/react/" target="_blank" rel="noopener noreferrer">React</a>를
+          사용하여 한 번 들어오면 같은 홈페이지 내에서는 새로운 페이지 로딩을 하지 않고 모든 내용을 볼 수 있습니다.
+          페이지 이동 이벤트를 발생시키면 주소창 내용만 바꾸고 새로운 요청을 날리지 않은 상태로 DOM 조작을 통해 페이지를 변경하는 식으로 페이지 이동을 처리합니다.
+          따라서 페이지를 로딩하면서 발생하는 깜빡임 없이 자연스럽게 애니메이션 등의 효과를 사용하면서 앱을 사용하는 것 같은 사용자 경험을 제공할 수 있습니다.
+        </p>
       </Paper>
     </div>
   );
