843444e6f54c968eaaa0d943cd1cd9f64e215345
Oh Jinkyun <tintypemolly@gmail.com>
2017-06-12 18:14:34 +0900

    credit page

--- forte-client/src/components/credit/index.js
+++ forte-client/src/components/credit/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card, CardHeader, CardText } from 'material-ui/Card';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+import HomeIcon from 'material-ui/svg-icons/action/home';
+import DescriptionIcon from 'material-ui/svg-icons/action/description';
 
 import forteImage from './forte_avatar.png';
 import hgyAvatar from './hwang_gyeongyun.jpeg';
@@ -8,6 +10,91 @@ import pyjAvatar from './park_yijae.jpeg';
 
 const Credit = () => {
   const cardStyle = {marginBottom: 10, marginRight: 10, display: 'inline-block'};
+  const thridParties = [
+    {
+      name: 'babel',
+      homepage: 'https://babeljs.io/',
+      license: 'https://github.com/babel/babel/blob/master/LICENSE',
+    }, {
+      name: 'express',
+      homepage: 'https://expressjs.com/',
+      license: 'https://github.com/expressjs/express/blob/master/LICENSE',
+    }, {
+      name: 'mysql',
+      homepage: 'https://github.com/mysqljs/mysql',
+      license: 'https://github.com/mysqljs/mysql/blob/master/License',
+    }, {
+      name: 'sequelize',
+      homepage: 'http://docs.sequelizejs.com/',
+      license: 'https://github.com/sequelize/sequelize/blob/master/LICENSE',
+    }, {
+      name: 'cheerio',
+      homepage: 'https://cheerio.js.org/',
+      license: 'https://github.com/cheeriojs/cheerio/blob/master/LICENSE',
+    }, {
+      name: 'esprima',
+      homepage: 'http://esprima.org/',
+      license: 'https://github.com/jquery/esprima/blob/master/LICENSE.BSD',
+    }, {
+      name: 'esprima-to-value',
+      homepage: 'https://github.com/yorkie/esprima-to-value',
+      license: 'https://github.com/yorkie/esprima-to-value/blob/master/LICENSE',
+    }, {
+      name: 'request',
+      homepage: 'https://github.com/request/request',
+      license: 'https://github.com/request/request/blob/master/LICENSE',
+    }, {
+      name: 'request-promise',
+      homepage: 'https://github.com/request/request-promise',
+      license: 'https://github.com/request/request-promise/blob/master/LICENSE',
+    }, {
+      name: 'geocoder',
+      homepage: 'https://github.com/wyattdanger/geocoder',
+      license: 'https://github.com/wyattdanger/geocoder/blob/master/LICENSE',
+    }, {
+      name: 'geodist',
+      homepage: 'https://github.com/cmoncrief/geodist',
+      license: 'https://github.com/cmoncrief/geodist/blob/master/LICENSE',
+    }, {
+      name: 'google-map-react',
+      homepage: 'https://github.com/istarkov/google-map-react',
+      license: null,
+      licenseRepr: 'MIT',
+    }, {
+      name: 'material-ui',
+      homepage: 'http://www.material-ui.com/',
+      license: 'https://github.com/callemall/material-ui/blob/master/LICENSE',
+    }, {
+      name: 'material-ui-responsive-drawer',
+      homepage: 'https://github.com/TarikHuber/material-ui-responsive-drawer',
+      license: null,
+      licenseRepr: 'MIT',
+    }, {
+      name: 'react',
+      homepage: 'https://facebook.github.io/react/',
+      license: 'https://github.com/facebook/react/blob/master/LICENSE',
+    }, {
+      name: 'react-router',
+      homepage: 'https://reacttraining.com/react-router/',
+      license: 'https://github.com/ReactTraining/react-router/blob/master/LICENSE.md',
+    }, {
+      name: 'react-router-redux',
+      homepage: 'https://github.com/reactjs/react-router-redux',
+      license: 'https://github.com/reactjs/react-router-redux/blob/master/LICENSE',
+    }, {
+      name: 'react-tap-event-plugin',
+      homepage: 'https://github.com/zilverline/react-tap-event-plugin',
+      license: 'https://github.com/zilverline/react-tap-event-plugin/blob/master/LICENSE',
+    }, {
+      name: 'redux',
+      homepage: 'http://redux.js.org/',
+      license: 'https://github.com/reactjs/redux/blob/master/LICENSE.md',
+    }, {
+      name: 'redux-responsive',
+      homepage: 'https://github.com/AlecAivazis/redux-responsive',
+      license: 'https://github.com/AlecAivazis/redux-responsive/blob/master/LICENSE.md',
+    },
+  ];
   return (
     <div style={{padding: 10}}>
       <h2>Members</h2>
@@ -28,7 +115,23 @@ const Credit = () => {
         <CardText>프로그래밍, 리소스 관리 담당</CardText>
       </Card>
       <h2>Open Source Usage Acknowledge</h2>
-      <p>TODO</p>
+      {
+        thridParties.map(pack => (
+          <div key={pack.name}>
+            <h3>{pack.name}</h3>
+            <ul style={{listStyle: 'none', padding: 0}}>
+              <li><HomeIcon/><a href={pack.homepage} target="_blank" rel="noopener noreferrer">Homepage</a></li>
+              <li><DescriptionIcon/>
+                {pack.license ?
+                  <a href={pack.license} target="_blank" rel="noopener noreferrer">License</a>
+                  :
+                  <span>{pack.licenseRepr} (라이센스 문서 없음)</span>
+                }
+              </li>
+            </ul>
+          </div>
+        ))
+      }
     </div>
   );
 };
